c9df3848bd60118c70f2d9270fdaf4fb4defcd8a
Pavilion <kirill24-974@mail.ru>
2022-08-02 17:44:49 +0300

    циклы . домашняя работа

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,24 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
 ///урок 1
+        {
+            int[] vesa = new int[]{111, 112, 113, 114, 115, 116, 117, 118,0 ,0, 0,0};
+            vesa[0] = 142;
+            int avgustWes = vesa[0];
+            System.out.println(avgustWes);
+            System.out.println(vesa[0]);
+            System.out.println(vesa[4]);
+            int avgust=0;
+            System.out.println(vesa[avgust]);
+            for (int i = 0; i < vesa.length; i++) {
+                System.out.println(vesa[i]);
+            }
+                
 
 
 
+        }
 
 
-
-
-
-    }
+        }
 }
